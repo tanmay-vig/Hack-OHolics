2c053ece54cef9b2b7aa975740e94af10fed4aee
Abhishek-2400 <111686252+Abhishek-2400@users.noreply.github.com>
2023-01-26 12:36:52 +0530

    Add files via upload

---
+++ src/components/Facilities.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import CardItem from './CardItem'
+import './Facilities.css'
+import pic1 from '../images/contract.jpg'
+import pic2 from '../images/link.jpg'
+
+
+function Facilities() {
+    return (
+        <div className='cards'>
+            <h1>Bla Bla Bla</h1>
+
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src={pic1}
+                            text="Terms and Conditions Processor - Gives you processed summary and main keywords of your agreements and contracts  "
+                            label='T&C PROCESSOR'
+                            path='/t&c'
+                        />
+                        <CardItem
+                            src={pic2}
+                            text="Authenticate Website urls and find whether they are fishy or Safe"
+                            label='Authenticate your links'
+                            path='/services'
+                        />
+
+
+                    </ul>
+
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Facilities
