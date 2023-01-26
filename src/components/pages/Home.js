618a77d3908af0add97b6a750a35d70298f15c47
Abhishek-2400 <111686252+Abhishek-2400@users.noreply.github.com>
2023-01-26 12:41:27 +0530

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Hero from '../Hero';
 import Cards from '../Cards';
 import Footer from '../Footer';
+import Facilities from '../Facilities';
 function Home() {
     return (
         <>
             <Hero />
             <Cards />
+            <Facilities />
             <Footer />
         </>
     )
