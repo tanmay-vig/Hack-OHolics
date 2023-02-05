9810e20bd7feb052aaba4cb646386c7b35681db9
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2023-02-05 10:17:12 +0530

    Update Facilities.js

--- src/components/Facilities.js
+++ src/components/Facilities.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import CardItem from './CardItem'
 import CardItem2 from './CardItem2'
 import './Facilities.css'
 import pic1 from '../images/tc-consulting.jpg'
-import pic2 from '../images/t2.jpg'
+import pic2 from '../images/NFT.jpg'
 
 
 function Facilities() {
@@ -19,11 +18,10 @@ function Facilities() {
                             text= <a href='http://127.0.0.1:5500/T_and_C_Formation/form.html'> Terms and Conditions Formation - Makes Terms and condition for you </a>
                             label='T&C Formation'
                         />
-                        <CardItem
+                        <CardItem2
                             src={pic2}
-                            text="Formulate your the Terms and Condition"
-                            label='T&C Formulation'
-                            path='/t&cf'
+                            text= <a href='https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/16057901895862908649661142886022720228921913520366323783314533470392866045953/'> Revenue </a>
+                            label='NFT Purchase'
                         />
 
 
